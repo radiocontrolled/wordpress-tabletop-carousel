63bc62ab05b0adc74558344f6dbd3103f0ede2ff
radiocontrolled <radiocontrolled@gmail.com>
2014-08-28 20:08:27 +0100

    add sample shortcode function for use in Wordpress

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ wordpress-tabletop-carousel
 ===========================
 
 A carousel for wordpress with data fed in from a Google spreadsheet
+This is based on the <a href="https://jlord.github.io/sheetsee.js/">Sheetsee.js</a> project. 
+
+See functions.php for a sample of how to insert the needed HTML markup in a wordpress theme, by using a shortcode. 
---
+++ functions.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+	 * Generate a shortcode to insert carousel markup
+	*/
+	function awards_shortcode_generator(){
+		return "<div class='row'>
+					<div class='col-sm-12 col-md-12'>
+						<section id='past-winners'>
+							<main id='pastWinnerViewer'>
+								<nav id='past-winnders-nav'>
+									<div class='collapse navbar-collapse' id='bs-example-navbar-collapse-1'>
+										<ul class='nav navbar-nav'>
+											<li class='dropdown'><a href='#' class='dropdown-toggle awards-year' data-toggle='dropdown'>Year<b class='caret'></b></a>
+												<ul class='dropdown-menu awards-dropdown-menu'></ul>
+											</li>
+										</ul>
+									</div>
+								</nav>
+							</main>
+						</section>
+					</div>
+				</div> ";
+	}
+	add_shortcode( 'awards_carousel', 'awards_shortcode_generator' );
+
+?>
